7995271c7a0ca58ba6b35444bd9d6ac5f20ad5f8
Drew Jaynes <xoodrew@gmail.com>
2026-04-17 22:14:09 -0600

    Lint.

--- bin/minify.php
+++ bin/minify.php
@@ -8,7 +8,7 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 use MatthiasMullie\Minify\CSS;
 use MatthiasMullie\Minify\JS;
@@ -18,7 +18,7 @@ $root = dirname(__DIR__);
 $assets = [
     'css' => [
         "{$root}/src/assets/css/debug-bar-console.css" => "{$root}/assets/css/debug-bar-console.min.css",
-        "{$root}/assets/css/iframe.css"                => "{$root}/assets/css/iframe.min.css",
+        "{$root}/assets/css/iframe.css" => "{$root}/assets/css/iframe.min.css",
     ],
     'js' => [
         "{$root}/src/assets/js/debug-bar-console.js" => "{$root}/assets/js/debug-bar-console.min.js",
