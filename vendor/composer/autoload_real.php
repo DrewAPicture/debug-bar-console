c4fe7bad41d116ab9ecbc377fbe7aee8e5deae26
Drew Jaynes <xoodrew@gmail.com>
2026-04-17 22:41:52 -0600

    No-dev for 1.0.0 release.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,6 @@ class ComposerAutoloaderInite55f8904505fa92165955bdfcc154f3d
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInite55f8904505fa92165955bdfcc154f3d::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
-
         return $loader;
     }
 }
